78a9d6b77b78b683146ad5e3588638f8eceae222
Andy Hartford <hartforda@gmail.com>
2015-10-18 16:51:54 -0400

    Things kinda work

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
-#![feature(libc)]
-extern crate libc;
-use libc::c_void;
-use libc::c_int;
-use std::ptr;
-//mod libtsm;
+extern crate pty;
+extern crate tsm_sys;
 
-#[repr(C)]
-struct TsmScreen;
-#[repr(C)]
-struct TsmLogT;
+mod program;
 
-// This code is editable and runnable!
+use program::*;
+use std::io;
+use std::io::Write;
+use std::io::Read;
+use std::os::unix::io::AsRawFd;
+
 fn main() {
-    #[link(name="tsm", kind="static")]
-    extern {
-        fn tsm_screen_new(out: *mut *mut TsmScreen, log: Option<TsmLogT>, log_data: *mut c_void) -> c_int;
-    }
+    let program = Program::new("vim".to_string()).unwrap();
 
-    unsafe {
-      let mut screen = ptr::null_mut();
-      tsm_screen_new(&mut screen, None, ptr::null_mut());
-    };
+    loop {
+            // blocking! Need to put the file handle in raw mode.
+            let mut input_buf  = [0; 10];
+            io::stdin().read(&mut input_buf);
+            println!("read from stdin {:?}", input_buf);
+
+            program.write(&input_buf);
+            println!("write to program");
+
+            let mut output_buf = [0; 10];
+            program.read(&mut output_buf);
+            println!("read from program {:?}", output_buf);
+
+            //let s = program.read_to_string();
+            //println!("read from program {}", s);
+
+            io::stdout().write(&output_buf);
+            println!("wrote to stdout");
+    }
 }
