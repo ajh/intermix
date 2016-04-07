55f15708dba6b7fa7c2be3a1984dee49c16c438e
Andy Hartford <hartforda@gmail.com>
2016-04-06 20:42:25 -0400

    Refactor drawing code

--- src/client/pen.rs
+++ src/client/pen.rs
@@ -3,22 +3,8 @@ use std::io::BufWriter;
 use term::terminfo::{parm, TermInfo};
 use vterm_sys::{self, Size, Pos, ColorPalette, ScreenCell, Rect, RectAssist};
 
-// TODO:
-//
-// * [ ] use terminfo for all writing
-// * [ ] set cursor visiblity in reset method
-// * [ ] audit other things missed in reset method
-// * [ ] DRY sgr code
-//
-// # Idea to DRY sgr code
-//
-// 1. Have constant defaults defined for attrs and stuff like BOLD = false;
-// 2. all values in Pen are Option
-// 3. pull out an sgr function that takes a pen, and writes bytes for Some(_) values
-// 4. reset adds Some(_) values for all attrs and writes sgr for everything
-// 5. draw cell builds up a pen with Some(_) values that are different than the current pen, then
-//    passes it to the sgr fn. Then updates the current pen.
-
+/// An object that tracks the state of the physical screen and collects desired changes to it. Can
+/// help make the desired changes reality by flushing them as a terminfo byte string.
 pub struct Pen {
     pub bg: ColorPalette,
     pub blink: bool,
@@ -47,9 +33,6 @@ pub struct Pen {
     disp_reverse: Option<bool>,
     disp_strike: Option<bool>,
     disp_underline: Option<u8>,
-
-    terminfo: TermInfo,
-    vars: parm::Variables,
 }
 
 impl Default for Pen {
@@ -81,27 +64,25 @@ impl Default for Pen {
             disp_reverse: None,
             disp_strike: None,
             disp_underline: None,
-            terminfo: TermInfo::from_name("xterm").unwrap(),
-            vars: parm::Variables::new(),
         }
     }
 }
 
 impl Pen {
-    pub fn new(terminfo: TermInfo) -> Pen {
-        Pen { terminfo: terminfo, .. Default::default() }
+    pub fn new() -> Pen {
+        Default::default()
     }
 
-    pub fn flush<W: Write>(&mut self, io: &mut W) {
-        let mut vars = parm::Variables::new();
-        let mut params: Vec<parm::Param> = vec![];
+    /// flush any accumulated changes and return a byte vec with the terminfo sequences
+    pub fn flush(&mut self, terminfo: &TermInfo, vars: &mut parm::Variables) -> Vec<u8> {
+        let mut bytes: Vec<u8> = Vec::new();
 
         if self.disp_pos.is_none() || *self.disp_pos.as_ref().unwrap() != self.pos {
             self.disp_pos = Some(self.pos);
 
-            params.push(parm::Param::Number(self.pos.y as i32));
-            params.push(parm::Param::Number(self.pos.x as i32));
-            Pen::write("cup", io, &self.terminfo, &mut vars, &mut params);
+            let params = vec![ parm::Param::Number(self.pos.y as i32),
+                 parm::Param::Number(self.pos.x as i32)];
+            self.apply_cap("cup", &mut bytes, terminfo, vars, &params);
         }
 
         // may have to reset here because there's no way to turn off stuff like bold through the
@@ -111,14 +92,15 @@ impl Pen {
         let need_reset_reverse = *self.disp_reverse.as_ref().unwrap_or(&true) && !self.reverse;
 
         if need_reset_bold || need_reset_blink || need_reset_reverse {
-            self.reset(io, &mut vars);
+            self.reset();
+            self.apply_cap("sgr0", &mut bytes, terminfo, vars, &vec![]);
         }
 
         if self.disp_bold.is_none() || *self.disp_bold.as_ref().unwrap() != self.bold {
             self.disp_bold = Some(self.bold);
 
             if self.bold {
-                Pen::write("bold", io, &self.terminfo, &mut vars, &mut params);
+                self.apply_cap("bold", &mut bytes, terminfo, vars, &vec![]);
             }
         }
 
@@ -126,9 +108,9 @@ impl Pen {
             self.disp_underline = Some(self.underline);
 
             if self.underline != 0 {
-                Pen::write("smul", io, &self.terminfo, &mut vars, &mut params);
+                self.apply_cap("smul", &mut bytes, terminfo, vars, &vec![]);
             } else {
-                Pen::write("rmul", io, &self.terminfo, &mut vars, &mut params);
+                self.apply_cap("rmul", &mut bytes, terminfo, vars, &vec![]);
             };
         }
 
@@ -136,9 +118,9 @@ impl Pen {
             self.disp_italic = Some(self.italic);
 
             if self.italic {
-                Pen::write("sitm", io, &self.terminfo, &mut vars, &mut params);
+                self.apply_cap("sitm", &mut bytes, terminfo, vars, &vec![]);
             } else {
-                Pen::write("ritm", io, &self.terminfo, &mut vars, &mut params);
+                self.apply_cap("ritm", &mut bytes, terminfo, vars, &vec![]);
             };
         }
 
@@ -146,7 +128,7 @@ impl Pen {
             self.disp_blink = Some(self.blink);
 
             if self.blink {
-                Pen::write("blink", io, &self.terminfo, &mut vars, &mut params);
+                self.apply_cap("blink", &mut bytes, terminfo, vars, &vec![]);
             }
         }
 
@@ -154,26 +136,30 @@ impl Pen {
             self.disp_reverse = Some(self.reverse);
 
             if self.reverse {
-                Pen::write("rev", io, &self.terminfo, &mut vars, &mut params);
+                self.apply_cap("rev", &mut bytes, terminfo, vars, &vec![]);
             }
         }
 
         if self.disp_fg.is_none() || *self.disp_fg.as_ref().unwrap() != self.fg {
             self.disp_fg = Some(self.fg);
 
-            params.push(parm::Param::Number(self.fg as i32));
-            Pen::write("setaf", io, &self.terminfo, &mut vars, &mut params);
+            let params = vec![ parm::Param::Number(self.fg as i32) ];
+            self.apply_cap("setaf", &mut bytes, terminfo, vars, &params);
         }
 
         if self.disp_bg.is_none() || *self.disp_bg.as_ref().unwrap() != self.bg {
             self.disp_bg = Some(self.bg);
 
-            params.push(parm::Param::Number(self.bg as i32));
-            Pen::write("setab", io, &self.terminfo, &mut vars, &mut params);
+            let params = vec![ parm::Param::Number(self.bg as i32) ];
+            self.apply_cap("setab", &mut bytes, terminfo, vars, &params);
         }
+
+        bytes
     }
 
-    fn reset<W: Write>(&mut self, io: &mut W, vars: &mut parm::Variables) {
+    /// reset the pen to the initial state where it doesn't think it knows anything about the
+    /// display state.
+    fn reset(&mut self) {
         self.disp_bg = Some(0);
         self.disp_blink = Some(false);
         self.disp_bold = Some(false);
@@ -186,13 +172,41 @@ impl Pen {
         self.disp_reverse = Some(false);
         self.disp_strike = Some(false);
         self.disp_underline = Some(0);
-        Pen::write("sgr0", io, &self.terminfo, vars, &mut vec![]);
     }
 
-    fn write<W: Write>(cap: &str, io: &mut W, ti: &TermInfo, vars: &mut parm::Variables, params: &mut Vec<parm::Param>) {
-        let bytes = apply_cap(cap, ti, vars, params);
-        io.write_all(&bytes).unwrap();
-        params.clear();
+    /// Updates many pen attributes from the given cell's attributes
+    pub fn update_attrs_from_cell(&mut self, cell: &ScreenCell) {
+        self.bg = cell.bg_palette;
+        self.blink = cell.attrs.blink;
+        self.bold = cell.attrs.bold;
+        self.fg = cell.fg_palette;
+        self.font = cell.attrs.font;
+        self.italic = cell.attrs.italic;
+        self.reverse = cell.attrs.reverse;
+        self.strike = cell.attrs.strike;
+        self.underline = cell.attrs.underline;
+    }
+
+    /// Let the pen know that the physical display advanced the cursor one position
+    pub fn notify_of_advanced_pos(&mut self, screen_size: &Size) {
+        if self.pos.x + 1 < screen_size.width {
+            self.pos.x += 1;
+        } else if self.pos.y + 1 < screen_size.height {
+            self.pos.x = 0;
+            self.pos.y += 1;
+        } else {
+            warn!("cursor is beyond screen size? What is supposed to happen here?");
+            self.pos.x = screen_size.width - 1;
+            self.pos.y = screen_size.height - 1;
+            self.disp_pos = None;
+        }
+    }
+
+    /// appends the cap sequence to the given bytes vec
+    fn apply_cap(&self, cap: &str, bytes: &mut Vec<u8>, terminfo: &TermInfo, vars: &mut parm::Variables, params: &Vec<parm::Param>) {
+        let cmd = terminfo.strings.get(cap).unwrap();
+        let mut sequence = parm::expand(&cmd, params.as_slice(), vars).unwrap();
+        bytes.append(&mut sequence);
     }
 }
 
@@ -200,9 +214,4 @@ impl ::std::fmt::Debug for Pen {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
         write!(f, "debug not implemented")
     }
-}
-
-fn apply_cap(cap: &str, terminfo: &TermInfo, vars: &mut parm::Variables, params: &Vec<parm::Param>) -> Vec<u8> {
-    let cmd = terminfo.strings.get(cap).unwrap();
-    parm::expand(&cmd, params.as_slice(), vars).unwrap()
 }
--- src/client/tty_painter.rs
+++ src/client/tty_painter.rs
@@ -5,35 +5,26 @@ use vterm_sys::{self, Size, Pos, ColorPalette, ScreenCell, Rect, RectAssist};
 use super::pen::*;
 
 // TODO:
-//
-// * [ ] use terminfo for all writing
-// * [ ] set cursor visiblity in reset method
-// * [ ] audit other things missed in reset method
-// * [ ] DRY sgr code
-//
-// # Idea to DRY sgr code
-//
-// 1. Have constant defaults defined for attrs and stuff like BOLD = false;
-// 2. all values in Pen are Option
-// 3. pull out an sgr function that takes a pen, and writes bytes for Some(_) values
-// 4. reset adds Some(_) values for all attrs and writes sgr for everything
-// 5. draw cell builds up a pen with Some(_) values that are different than the current pen, then
-//    passes it to the sgr fn. Then updates the current pen.
+// * [ ] clean up error handling and all the expects on write_all
 
-#[derive(Debug)]
 pub struct TtyPainter<F: Write + Send> {
     // the physical state of the tty that is being painted
     pen: Pen,
     io: BufWriter<F>,
     size: Size,
+    terminfo: TermInfo,
+    vars: parm::Variables,
 }
 
 impl<F: Write + Send> TtyPainter<F> {
     pub fn new(io: F, size: Size) -> TtyPainter<F> {
         TtyPainter {
             io: BufWriter::new(io),
-            pen: Pen::new(TermInfo::from_env().unwrap()),
+            pen: Pen::new(),
             size: size,
+            // Note: a better idea would be to have the caller choose the terminal type
+            terminfo: TermInfo::from_env().unwrap(),
+            vars: parm::Variables::new(),
         }
     }
 
@@ -43,8 +34,7 @@ impl<F: Write + Send> TtyPainter<F> {
 
         let terminfo = TermInfo::from_env().unwrap();
         let mut vars = parm::Variables::new();
-        let bytes = apply_cap("sc", &terminfo, &mut vars, &vec![]);
-        self.io.write_all(&bytes).unwrap();
+        self.write_cap("sc", &vec![]);
 
         for (cell, pos) in cells.iter().zip(rect.positions()) {
             if pos.x >= self.size.width || pos.y >= self.size.height {
@@ -54,56 +44,30 @@ impl<F: Write + Send> TtyPainter<F> {
             }
 
             self.pen.pos = pos;
-            self.draw_cell(cell);
+            self.pen.update_attrs_from_cell(&cell);
+            let bytes = self.pen.flush(&self.terminfo, &mut self.vars);
+            self.io.write_all(&bytes).ok().expect("failed to write");
+
+            // See tmux's tty.c:1155 function `tty_cell`
+            if cell.chars.len() > 0 {
+                self.io.write_all(&cell.chars).ok().expect("failed to write");
+            } else {
+                // like tmux's tty_repeat_space
+                self.io.write_all(&[b'\x20']).ok().expect("failed to write"); // space
+            }
+
+            self.pen.notify_of_advanced_pos(&self.size);
+
+            if cell.width > 1 {
+                warn!("cell has width > 1 {:?}, but acting on this information isnt implemented",
+                      cell)
+            }
         }
 
-        let bytes = apply_cap("rc", &terminfo, &mut vars, &vec![]);
-        self.io.write_all(&bytes).unwrap();
+        self.write_cap("rc", &vec![]);
 
         self.io.flush().unwrap();
         trace!("draw_cells finish");
-    }
-
-    // This method assumes the pen is already at the correct position
-    fn draw_cell(&mut self, cell: &ScreenCell) {
-        //trace!("draw_cell cell={:?} pen.pos={:?}", cell, self.pen.pos);
-
-        self.pen.bg = cell.bg_palette;
-        self.pen.blink = cell.attrs.blink;
-        self.pen.bold = cell.attrs.bold;
-        self.pen.fg = cell.fg_palette;
-        self.pen.font = cell.attrs.font;
-        self.pen.italic = cell.attrs.italic;
-        self.pen.reverse = cell.attrs.reverse;
-        self.pen.strike = cell.attrs.strike;
-        self.pen.underline = cell.attrs.underline;
-
-        self.pen.flush(&mut self.io);
-
-        // See tmux's tty.c:1155 function `tty_cell`
-        if cell.chars.len() > 0 {
-            self.io.write_all(&cell.chars).ok().expect("failed to write");
-        } else {
-            // like tmux's tty_repeat_space
-            self.io.write_all(&[b'\x20']).ok().expect("failed to write"); // space
-        }
-
-        if self.pen.pos.x + 1 < self.size.width {
-            self.pen.pos.x += 1;
-        } else if self.pen.pos.y + 1 < self.size.height {
-            self.pen.pos.x = 0;
-            self.pen.pos.y += 1;
-        } else {
-            warn!("cursor is beyond screen size? What is supposed to happen here?");
-            // for now just wedge it into the bottom right corner
-            self.pen.pos.x = self.size.width - 1;
-            self.pen.pos.y = self.size.height - 1;
-        }
-
-        if cell.width > 1 {
-            warn!("cell has width > 1 {:?}, but acting on this information isnt implemented",
-                  cell)
-        }
     }
 
     /// TODO: take a offset from the pane
@@ -111,7 +75,7 @@ impl<F: Write + Send> TtyPainter<F> {
         trace!("move_cursor pos={:?} is_visible={:?}", pos, is_visible);
         self.pen.pos = pos;
         self.pen.is_visible = is_visible;
-        self.pen.flush(&mut self.io);
+        self.io.write_all(&self.pen.flush(&self.terminfo, &mut self.vars)).ok().expect("failed to write");
     }
 
     /// Implemented like tmux's tty_redraw_region
@@ -127,16 +91,17 @@ impl<F: Write + Send> TtyPainter<F> {
     }
 
     pub fn flush(&mut self) {
-        self.pen.flush(&mut self.io);
+        self.io.write_all(&self.pen.flush(&self.terminfo, &mut self.vars)).ok().expect("failed to write");
         self.io.flush();
     }
 
     // pub fn delete_line<F: Write>(&mut self, pane: &Pane, io: &mut F) {
     // /deleteLine: CSR(top, bottom) + CUP(y, 0) + DL(1) + CSR(0, height)
     // }
-}
 
-fn apply_cap(cap: &str, terminfo: &TermInfo, vars: &mut parm::Variables, params: &Vec<parm::Param>) -> Vec<u8> {
-    let cmd = terminfo.strings.get(cap).unwrap();
-    parm::expand(&cmd, params.as_slice(), vars).unwrap()
+    fn write_cap(&mut self, cap: &str, params: &Vec<parm::Param>) {
+        let cmd = self.terminfo.strings.get(cap).unwrap();
+        let mut bytes = parm::expand(&cmd, params.as_slice(), &mut self.vars).unwrap();
+        self.io.write_all(&bytes).ok().expect("failed to write");
+    }
 }
